a7ec33f40fb529d5ffa65963e272a47ec5ed13a3
Joakim-animate90 <joakimbwire23@gmail.com>
2022-04-26 01:06:14 -0700

    2-add_nodeint.c

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,3 +32,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+#endif
