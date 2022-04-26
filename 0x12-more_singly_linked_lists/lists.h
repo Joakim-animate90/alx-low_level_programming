b9e069620d3bd484068f1c544d73efcfaf8db4b3
Joakim-animate90 <joakimbwire23@gmail.com>
2022-04-26 01:02:00 -0700

    0-print_listint.c

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,6 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -33,5 +30,3 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif
